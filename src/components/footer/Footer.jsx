03221a6293ed0427828b97719473c3ac4a5a4c4e
fatih-design <tukenmezfatih@gmail.com>
2025-01-11 16:16:43 +0100

    De Header en Footer components en Home page aangemaakt.

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <>
+            <h4> ©Copyright Fatih Tukenmez Design™ </h4>
+
+        </>
+    );
+
+}
+
+export default Footer;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css';
+import Header from "../../components/header/Header.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+
+function Home(props) {
+
+    return (
+        <>
+           <Header/>
+           <Footer/>
+        </>
+    );
+
+}
+
+export default Home;
